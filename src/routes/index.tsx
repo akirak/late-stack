90b13a8da51249f9dd159cf4a67da48e43b39a4a
Akira Komamura <akira.komamura@gmail.com>
2025-06-16 21:25:25 +0900

    Add post list

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,21 @@
 import { createFileRoute } from "@tanstack/react-router"
+import { getPostList } from "@/collections/posts"
+import { PostListTable } from "@/features/blog/components/PostListTable"
 
 export const Route = createFileRoute("/")({
   component: HomeComponent,
+  loader: async () => {
+    return { posts: await getPostList({}) }
+  },
 })
 
 function HomeComponent() {
+  const { posts } = Route.useLoaderData()
   return (
     <main>
-      <h1>Hello world!</h1>
+      <h1>jingsi.space</h1>
+
+      <PostListTable posts={posts} />
     </main>
   )
 }
