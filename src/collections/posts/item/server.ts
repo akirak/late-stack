1a9c2dfe7143093a3ffc97eb7fac38e17e59b152
Akira Komamura <akira.komamura@gmail.com>
2025-06-23 05:10:50 +0900

    fix: Fix loading

--- src/collections/posts/item/server.ts
+++ src/collections/posts/item/server.ts
@@ -1,11 +1,12 @@
 import type { PostSpec } from "./types"
-import { pipe, Schema } from "effect"
+import { Option, pipe, Schema } from "effect"
 import { PostSchema } from "@/schemas/post"
 import { getPostInternalSync } from "./internal"
 
 export function getPostSync(data: typeof PostSpec.Type) {
   return pipe(
     getPostInternalSync(data),
+    Option.getOrThrow,
     Schema.decodeUnknownSync(PostSchema),
   )
 }
