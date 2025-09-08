9c33387c59d01baae06931af427257d6d7dd415f
Akira Komamura <akira.komamura@gmail.com>
2025-09-08 12:16:16 +0900

    style: Reformat

--- src/collections/languages.ts
+++ src/collections/languages.ts
@@ -6,17 +6,17 @@ import { LanguagePropertiesSchema } from "@/schemas/common"
 type LanguageProperties = typeof LanguagePropertiesSchema.Type
 
 const UsedLanguages: readonly LanguageProperties[]
-      = Schema.decodeUnknownSync(Schema.Array(LanguagePropertiesSchema))([
-        {
-          id: "en",
-          englishName: "English",
-        },
-        {
-          id: "ja",
-          localName: "日本語",
-          englishName: "Japanese",
-        },
-      ])
+  = Schema.decodeUnknownSync(Schema.Array(LanguagePropertiesSchema))([
+    {
+      id: "en",
+      englishName: "English",
+    },
+    {
+      id: "ja",
+      localName: "日本語",
+      englishName: "Japanese",
+    },
+  ])
 
 export function getLanguageById(id: LanguageId): Option.Option<LanguageProperties> {
   return pipe(
